3ce01057d28a2cd5be2c541d650414555b05ce86
shayday <shay.dayan1007@gmail.com>
2017-01-04 14:21:11 +0200

    adding MvcProjects and 2 example for using asp.net mvc5

---
+++ Mvc Projects/MVCLogin/MVCLogin/Controllers/AccountController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCLogin.Models;
+
+namespace MVCLogin.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+        public ActionResult Index()
+        {
+            using (OurDBContext db = new OurDBContext())
+            {
+                return View(db.userAccount.ToList());
+            }
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserAccount usr)
+        {
+            if (ModelState.IsValid)
+            {
+                using (OurDBContext db = new OurDBContext())
+                {
+                    db.userAccount.Add(usr);
+                    db.SaveChanges();
+                }
+                ModelState.Clear();
+                ViewBag.Messege = usr.FirstName + " " + usr.LastName + " successfully registared.";
+            }
+            return View();
+        }
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(UserAccount user)
+        {
+            using(OurDBContext db = new OurDBContext())
+            {
+                var usr = db.userAccount.Single(u => u.FirstName == user.FirstName && u.Password == user.Password);
+                if (usr != null)
+                {
+                    Session["UserID"] = usr.UserID.ToString();
+                    Session["Username"] = usr.Username.ToString();
+                    return RedirectToAction("LoggedIn");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Username or Password is wrong.");
+                }
+            }
+            return View();
+        }
+
+        public ActionResult LoggedIn()
+        {
+            if(Session["UserID"] !=null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+           
+	}
+}
---
+++ Mvc Projects/WebApplication1/WebApplication1/ModelView/CustomerViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace WebApplication1.ModelView
+{
+    public class CustomerViewModel
+    {
+        [Required]
+        public Customer customer { get; set; }
+
+        public List<Customer> customers { get; set; }
+    }
+}
