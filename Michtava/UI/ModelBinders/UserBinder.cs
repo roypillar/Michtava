4f7a95101c416bc544eefbb2b73a20daf9c8758f
quwala <quwala@gmail.com>
2017-01-12 16:00:14 +0200

    Changed Customer to user, trying to clean all extras.

---
+++ Michtava/UI/ModelBinders/UserBinder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain.Models;
+
+namespace UI.ModelBinders
+{
+    public class UserBinder : IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            HttpContextBase objContext = controllerContext.HttpContext;
+            string uFirstName = objContext.Request.Form["user.FirstName"];
+            string uLastName = objContext.Request.Form["user.LastName"];
+            string uNumber = objContext.Request.Form["user.UserNumber"];
+
+            User obj = new User 
+                                        {FirstName = uFirstName,
+                                         LastName = uLastName,
+                                         UserNumber = uNumber
+                                        };
+
+            return obj;
+        }
+    }
+}
