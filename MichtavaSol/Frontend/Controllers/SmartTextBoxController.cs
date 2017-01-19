3644c5ecbd8e4979167961d7a8956a017e9396e5
OT\MWeiss <magal@post.bgu.ac.il>
2017-01-19 15:27:24 +0200

    SmartTextBox - UI

---
+++ MichtavaSol/Frontend/Controllers/SmartTextBoxController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Frontend.Controllers
+{
+    public class SmartTextBoxController : Controller
+    {
+        public ActionResult SmartTextBox()
+        {
+            ViewBag.Title = "תיבת טקסט חכמה";
+
+            return View();
+        }
+
+        public ActionResult AnalyzeAnswer()
+        {
+            // here we have to call the SmartTextBox in server side
+
+            TempData["Answer"] = Request.Form["myTextBox2"];
+
+            return RedirectToAction("SmartTextBox");
+        }
+    }
+}
