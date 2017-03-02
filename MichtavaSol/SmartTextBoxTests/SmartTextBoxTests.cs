460de86781ba00e9e7c180171c248f61678cf0fd
OT\MWeiss <magal@post.bgu.ac.il>
2017-03-02 16:15:38 +0200

    Template for SmartTextBox tests + compilation fixes

---
+++ MichtavaSol/SmartTextBoxTests/SmartTextBoxTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmartTextBox;
+
+namespace SmartTextBoxTests
+{
+    [TestClass]
+    public class SmartTextBoxTests
+    {
+        private ISmartTextBox _smartTextBox;
+        private string _text;
+
+        [TestMethod]
+        public void GetNumberOfConnectorsTests()
+        {
+            _smartTextBox = new SmartTextBoxImpl();
+            _text = "";
+        }
+
+        [TestMethod]
+        public void GetNumberOfWordsTests()
+        {
+            _smartTextBox = new SmartTextBoxImpl();
+            _text = "";
+        }
+
+        [TestMethod]
+        public void GetRepeatedWordsTests()
+        {
+            _smartTextBox = new SmartTextBoxImpl();
+            _text = "";
+        }
+
+        [TestMethod]
+        public void SuggestAlternativeWordTests()
+        {
+            _smartTextBox = new SmartTextBoxImpl();
+            _text = "";
+        }
+
+        [TestMethod]
+        public void CheckPunctuatioMarksTests()
+        {
+            _smartTextBox = new SmartTextBoxImpl();
+            _text = "";
+        }
+    }
+}
