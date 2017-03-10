e5fbdc92cc4f7d47a8bd5eee9d3c37f6bc50f00b
shayday <shay.dayan1007@gmail.com>
2017-03-10 14:11:11 +0200

    smartbox improvement, numOfWords and connectors and stuff

--- MichtavaSol/Frontend/Controllers/SmartTextBoxController.cs
+++ MichtavaSol/Frontend/Controllers/SmartTextBoxController.cs
@@ -23,6 +23,7 @@ namespace Frontend.Controllers
             {
                 TempData["NumberOfWords"] = "0";
                 TempData["NumberOfConnectorWords"] = "0";
+                TempData["toManyWords"] = "";
             }
 
 
@@ -44,11 +45,28 @@ namespace Frontend.Controllers
             // here we have to call the SmartTextBox in server side
 
             string input = Request.Form["TextBoxArea"];
-
-            TempData["NumberOfWords"] = _smartTextBox.GetNumberOfWords(input);
-            TempData["NumberOfConnectorWords"] = _smartTextBox.GetNumberOfConnectors(input);
+            int numOfWords = _smartTextBox.GetNumberOfWords(input);
+            int numOfConnectors = _smartTextBox.GetNumberOfConnectors(input);
+            TempData["NumberOfWords"] = numOfWords;
+            TempData["NumberOfConnectorWords"] = numOfConnectors;
             TempData["Answer"] = input;
 
+            //כשנוסיף את הפוליסי שתרוץ לא תהיה כנראה את הבעיה.. בינתיים
+            Policy _policy = new Policy();
+            List<string> _keySentencesList = new List<string>();
+            _keySentencesList.Add("התשובה לשאלה שנשאלה היא");
+            _policy = new Policy() { _id = "1", _minWords = 20, _maxWords = 30, _minConnectors = 3, _maxConnectors = 8, _keySentences = _keySentencesList };
+
+            if(numOfWords>_policy._maxWords)
+            {
+                TempData["toManyWords"] = "הכנסת " + numOfWords + " מילים, אבל מותר לכל היותר " + _policy._maxWords + " מילים.";
+            }
+            if (numOfConnectors > _policy._maxConnectors)
+            {
+                TempData["toManyConnectors"] = "הכנסת " + numOfConnectors + " מילות קישור, אבל מותר לכל היותר " + _policy._maxConnectors + " מילות קישור.";
+            }
+            
+            
             return RedirectToAction("SmartTextBox", _policy);
         }
     }
