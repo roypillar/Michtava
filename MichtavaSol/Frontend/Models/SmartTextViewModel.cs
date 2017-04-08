67cc2149e4bd71331f7ebd2aedf5344fed246bf5
shayday <shay.dayan1007@gmail.com>
2017-04-08 15:36:43 +0300

    Questions view and smart text box View model

---
+++ MichtavaSol/Frontend/Models/SmartTextViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Entities.Models;
+
+
+
+namespace Frontend.Models
+{
+    public class SmartTextViewModel
+    {
+        public Text text { get; set; }
+
+        public List<Question> questions { get; set; }
+
+        public Policy policy { get; set; }
+    }
+}
