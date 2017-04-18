ad75e828cedacac962a369d159dda8b7eae98402
quwala <quwala@gmail.com>
2017-04-18 19:14:04 +0300

    added tests

--- MichtavaSol/Dal_Tests/AdministratorRepositoryTests.cs
+++ MichtavaSol/Dal_Tests/AdministratorRepositoryTests.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit;
 
 namespace Dal_Tests
 {
-    [TestClass]
+    
     class AdministratorRepositoryTests
     {
 
--- MichtavaSol/Dal_Tests/GenericRepositoryTests.cs
+++ MichtavaSol/Dal_Tests/GenericRepositoryTests.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using Dal;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using Entities.Models;
 
 namespace Dal_Tests
 {
+    [TestFixture]
     class GenericRepositoryTests
     {
+        private IApplicationDbContext ctx;
+
+
+        [SetUp]
+        void setUp() { 
+        
+            this.ctx = ApplicationDbContext.Create();
+        }
+
+        void testWithAnswers()
+        {
+            IDbSet<Answer> set = ctx.Set<Answer>();
+        }
     }
+    
 }
