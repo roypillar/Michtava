1f975c1e75f39abd5b3c18d170314d3d32cc60c0
quwala <quwala@gmail.com>
2017-05-17 13:49:25 +0300

    interface for all IDable entities, updated diagram arrow

---
+++ MichtavaSol/Entities/Models/HasId.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    interface HasId
+    {
+         Guid Id { get; set; }
+    }
+}
